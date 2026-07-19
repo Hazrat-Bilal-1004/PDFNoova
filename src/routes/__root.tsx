ee09a802276fc2e3107f0e7231b7fe2e469a4d25
Hazrat Bilal <fbayt8800@gmail.com>
2026-07-19 14:23:47 +0500

    Remove canonical link from root routes
    Removed canonical link to the PDF Noova website.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -99,11 +99,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "stylesheet", href: appCss },
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "alternate icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "canonical",
-        href:
-          "https://www.pdfnoova.com/",
-      },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
